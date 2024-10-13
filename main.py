f798155cb2e4c82675f85c8e770239e6472160f9
Roman Slonkin <romanslonkin@gmail.com>
2024-10-13 21:45:02 +0300

    2

--- main.py
+++ main.py
@@ -3,7 +3,18 @@
 
 # print(mymodule.say_hello("World"))
 
-# main.py
-from mymodule import say_hello as greeting
+# # main.py
+# from mymodule import say_hello as greeting
 
-print(greeting("World"))
+# print(greeting("World"))
+
+# import sys
+# import os
+
+# print(sys.modules["os"])
+
+import sys
+import os
+
+# print(sys.modules.keys())
+print(sys.builtin_module_names)
