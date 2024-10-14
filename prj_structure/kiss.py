695dd85ad3d51d00c8899623a54e64bfac3abecf
Roman Slonkin <romanslonkin@gmail.com>
2024-10-14 16:15:05 +0300

    kiss

--- prj_structure/kiss.py
+++ prj_structure/kiss.py
@@ -15,16 +15,31 @@
 # print(is_even(15))
 #################################
 
-def is_palindrome(s: str) -> bool:
-    new_s = ""
-    for char in s:
-        if char.isalnum():
-            new_s += char.lower
-    s = new_s
-    length = len(s)
-    for i in range(length//2):
-        if s[i] != s[length-i-1]:
-            return False
-    return True
+# def is_palindrome(s: str) -> bool:
+#     new_s = ""
+#     for char in s:
+#         if char.isalnum():
+#             new_s += char.lower
+#     s = new_s
+#     length = len(s)
+#     for i in range(length//2):
+#         if s[i] != s[length-i-1]:
+#             return False
+#     return True
 
-print(is_palindrome("Козак з казок"))
+# print(is_palindrome("Козак з казок"))
+
+########### THE SAME AS:
+
+# def is_palindrome(s: str) -> bool:
+#     new_s = ""
+#     for char in s:
+#         if char.isalnum():
+#             new_s += char.lower()
+
+#     s = new_s
+#     return s == s[::-1]
+
+# print(is_palindrome("Козак з казок"))  
+
+ 
